cf057d99225ab097c7b643fab247a387577e1da3
Eugene Lysov <evg.lysov@gmail.com>
2020-11-22 16:35:33 +0200

    1

--- src/fetchCountries.js
+++
@@ -1,6 +0,0 @@
-const url = 'https://restcountries.eu/rest/v2/name/';
-export default function fetchCountries(searchQuery) {
-  return fetch(url + searchQuery)
-    .then(response => response.json())
-    .catch(err => console.log(err));
-}
